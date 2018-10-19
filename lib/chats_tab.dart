4a1e263689c16572b28bd61614fee646edd9b6db
sandipghosh <sandipgh19@gmail.com>
2018-10-19 21:49:12 +0530

    chat items

--- lib/chats_tab.dart
+++ lib/chats_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
@@ -6,9 +8,21 @@ void main() {
 }
 
 class ChatsTab extends StatelessWidget {
+
+  List<ListItem> items;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
   }
+}
 
+abstract class ListItem {}
+
+class ChatItem implements ListItem {
+  String title;
+  String subtitle;
+  Image profile;
+
+  ChatItem(this.title, this.subtitle, this.profile);
 }
